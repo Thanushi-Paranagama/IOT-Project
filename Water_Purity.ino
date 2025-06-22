7e428924bddb40458192e5d376184375e77f1c24
H M Thanushi Paranagama <thanushiparanagama@gmail.com>
2025-06-22 22:11:13 +0530

    Update Water_Purity.ino

--- Water_Purity.ino
+++ Water_Purity.ino
@@ -1,37 +1,104 @@
-#define TdsSensorPin A0
+#include <ESP8266WiFi.h>
 
-float adcVoltage = 0;
+#include <Firebase_ESP_Client.h>
+ 
+// Wi-Fi credentials
+#define WIFI_SSID "H M T's A55"
+#define WIFI_PASSWORD "qwer1234"
+ 
+// Firebase project details
 
-float tdsValue = 0;
+#define API_KEY "AIzaSyD6Cs9LM-y6gBkvDGlDAyn9VPi3F8Wd3uw"
+#define DATABASE_URL "https://monitoring-water-system-ca40a-default-rtdb.asia-southeast1.firebasedatabase.app/"  // MUST end with '/'
+ 
+// TDS Sensor Pin
+
+#define TDS_PIN A0
+ 
+// Firebase objects
+
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+
+FirebaseConfig config;
+ 
+unsigned long lastSendTime = 0;
  
 void setup() {
 
   Serial.begin(9600);
+ 
+  // Connect to Wi-Fi
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+
+    delay(500);
+
+    Serial.print(".");
+
+  }
+
+  Serial.println("\nWiFi connected.");
+ 
+  // Firebase Configuration
+
+  config.api_key = API_KEY;
+
+  config.database_url = DATABASE_URL;
+ 
+  // Anonymous sign-in
+
+  if (Firebase.signUp(&config, &auth, "", "")) {
+
+    Serial.println("Anonymous sign-in successful.");
+
+  } else {
+
+    Serial.printf("Sign-up error: %s\n", config.signer.signupError.message.c_str());
+
+  }
+ 
+  Firebase.begin(&config, &auth);
+
+  Firebase.reconnectWiFi(true);
 
 }
  
 void loop() {
 
-  int analogValue = analogRead(TdsSensorPin);
+  if (millis() - lastSendTime > 10000) {
 
-  adcVoltage = analogValue * (3.3 / 1024.0); // Convert analog to voltage (ESP8266 = 10-bit ADC)
-
-  // Formula: TDS (ppm) = (133.42*V^3 - 255.86*V^2 + 857.39*V) * 0.5
-
-  // The "*0.5" is the TDS factor; you can calibrate it
-
-  float voltage = adcVoltage;
-
-  tdsValue = (133.42 * pow(voltage, 3) - 255.86 * pow(voltage, 2) + 857.39 * voltage) * 0.5;
+    lastSendTime = millis();
  
-  Serial.print("TDS Value: ");
+    int analogValue = analogRead(TDS_PIN);
 
-  Serial.print(tdsValue);
+    float voltage = analogValue * (1.0 / 1023.0);  // For ESP8266 (0-1V ADC)
 
-  Serial.println(" ppm");
+    float tdsValue = (133.42 * pow(voltage, 3) - 255.86 * pow(voltage, 2) + 857.39 * voltage) * 0.5;
  
-  delay(1000);
+    String path = "/tds_data/" + String(millis());
+ 
+    Serial.print("Sending TDS: ");
+
+    Serial.println(tdsValue);
+ 
+    if (Firebase.RTDB.setFloat(&fbdo, path.c_str(), tdsValue)) {
+
+      Serial.println("TDS value uploaded successfully.");
+
+    } else {
+
+      Serial.print("Firebase error: ");
+
+      Serial.println(fbdo.errorReason());
+
+    }
+
+  }
 
 }
-
- 
